c5818324c0dbd0f15b1336a44a39bd8b2d1d95ce
Anik Das <onikd08@gmail.com>
2023-10-31 06:05:17 +0200

    Add bookmark button

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -23,6 +23,20 @@ const Blog = ({ blog }) => {
         </div>
         <div>
           <small className="text-gray-400">{reading_time} min read</small>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-5 inline text-gray-400"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+            />
+          </svg>
         </div>
       </div>
       <h2 className="text-3xl mt-5">{title}</h2>
