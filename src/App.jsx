899f2d3d6aeab5da04fbe290d4bd72ba1f6ddcd8
Anik Das <onikd08@gmail.com>
2023-10-31 07:26:23 +0200

    Move reading time div into Bookmark component

--- src/App.jsx
+++ src/App.jsx
@@ -25,14 +25,7 @@ function App() {
           handleBookmark={handleBookmark}
           handleMarkAsRead={handleMarkAsRead}
         ></Blogs>
-        <div className="md:w-1/3">
-          <div className="text-center border py-5 mb-5 border-[#6047EC] bg-[#6047ec1a] rounded-xl">
-            <h3 className="text-xl font-semibold text-[#6047EC]">
-              Spent time on reading: {totalTime} min
-            </h3>
-          </div>
-          <Bookmarks bookmarks={bookmarks}></Bookmarks>
-        </div>
+        <Bookmarks bookmarks={bookmarks} totalTime={totalTime}></Bookmarks>
       </main>
     </div>
   );
--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,19 +1,29 @@
 import PropTypes from "prop-types";
 
-const Bookmarks = ({ bookmarks }) => {
+const Bookmarks = ({ bookmarks, totalTime }) => {
   return (
-    <div className="bg-slate-100 p-7 rounded-xl">
-      <h3 className="text-2xl mb-4">Bookmarked Blogs: {bookmarks.length}</h3>
-      {bookmarks.map((bookmark_title, idx) => (
-        <div key={idx} className="p-5 bg-white mb-5 rounded-lg">
-          <p className="font-semibold">{bookmark_title}</p>
+    <div className="md:w-1/3">
+      <div>
+        <div className="text-center border py-5 mb-5 border-[#6047EC] bg-[#6047ec1a] rounded-xl">
+          <h3 className="text-xl font-semibold text-[#6047EC]">
+            Spent time on reading: {totalTime} min
+          </h3>
         </div>
-      ))}
+      </div>
+      <div className="bg-slate-100 p-7 rounded-xl">
+        <h3 className="text-2xl mb-4">Bookmarked Blogs: {bookmarks.length}</h3>
+        {bookmarks.map((bookmark_title, idx) => (
+          <div key={idx} className="p-5 bg-white mb-5 rounded-lg">
+            <p className="font-semibold">{bookmark_title}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
 Bookmarks.propTypes = {
   bookmarks: PropTypes.array.isRequired,
+  totalTime: PropTypes.number.isRequired,
 };
 export default Bookmarks;
